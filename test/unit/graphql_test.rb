8c5995ef2c43937a3d721b2f06d9b8f5a10b988b
willian <mail@willian.io>
2017-05-20 16:53:26 +0200

    @esjee fixing things

--- test/unit/graphql_test.rb
+++ test/unit/graphql_test.rb
@@ -3,6 +3,40 @@ require 'test_helper'
 class GraphQLUnitTest < Minitest::Test
   include Liquid
 
+
+  class Thing
+    attr_reader :thing, :children
+    def initialize(thing, children)
+      @thing = thing
+      @children = children || []
+    end
+
+    def merge_with(other_thing)
+      @children += [other_thing.children]
+#      children |= other_thing.children
+      self
+    end
+
+    def to_graphql
+      attributes_to_graphql([thing] + children)
+    end
+
+    private
+
+    def attributes_to_graphql(attributes)
+      thing, *children = attributes
+      if children.size == 1 && children[0].is_a?(Array)
+        binding.pry
+        "#{thing} #{attributes_to_graphql children[0]}"
+      elsif children.size > 0
+        "#{thing} { #{attributes_to_graphql children} }"
+      else
+        thing
+      end
+    end
+  end
+
+
   def test_to_query
     def magic(template)
       template = Liquid::Template.parse(template)
@@ -21,30 +55,48 @@ class GraphQLUnitTest < Minitest::Test
 
       def transform_tag_to_graphql(lookups)
         attribute, *deep_attributes = lookups
-        if deep_attributes.any?
-          # warning: recursion!
-          "#{attribute} { #{transform_tag_to_graphql deep_attributes} }"
-        else
-          attribute
-        end
+        Thing.new(attribute, deep_attributes)
       end
 
       fragments = Hash.new {|h,k| h[k] = [] }
       tags.each do |tag|
-        fragments[tag.name.name] << transform_tag_to_graphql(tag.name.lookups)
+        #children = Hash.new {|h,k| h[k] = [] }
+
+        existing_fragments = fragments[tag.name.name]
+        new_fragment = transform_tag_to_graphql(tag.name.lookups)
+
+        if existing_fragments.size > 0
+          merged = false
+          existing_fragments_merged_with_news = existing_fragments.map do |fragment|
+            if fragment.thing == new_fragment.thing
+              merged = true
+              fragment.merge_with(new_fragment)
+            else
+              fragment
+            end
+          end
+
+          unless merged
+            existing_fragments_merged_with_news << new_fragment
+          end
+
+          fragments[tag.name.name] = existing_fragments_merged_with_news
+        else
+          fragments[tag.name.name] << new_fragment
+        end
       end
 
       fragments.map do |k, v|
          <<-EOS
 fragment on #{k.capitalize} {
-  #{v.join(" ")}
+  #{v.map(&:to_graphql).join(" ")}
 }
       EOS
       end
     end
 
     template = %{
-      <h1>Hi {{ user.name }} from {{ user.address.city }}</h1>
+      <h1>Hi {{ user.name }} from {{ user.address.city }}, {{ user.address.country.iso }}</h1>
       <ul id="products">
         {% for product in products %}
           <li>
@@ -59,7 +111,7 @@ fragment on #{k.capitalize} {
 
     user_fragment = <<-EOS
 fragment on User {
-  name address { city }
+  name address { city country { iso } }
 }
       EOS
 
